4c693ba5b4e61c4481b03aed21eedd8904d0c694
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-10-14 14:13:39 +0500

    Projects section Separate file set and layout complete

---
+++ src/Pages/Project.tsx
@@ -0,0 +1,38 @@
+import { Content } from "antd/es/layout/layout";
+import React from "react";
+
+function Project() {
+  return (
+    <Content style={{ paddingRight: "60px" }}>
+      <Content
+        style={{
+          paddingTop: "90px",
+          paddingRight: "60px",
+          position: "relative",
+        }}
+      >
+        <Content className="Project-section" style={{ background: "#0EA5E9" }}>
+          <Content style={{ backgroundColor: "" }}>
+            <div className="Project-1">PRO</div>
+            <div className="Project-2">JECTS</div>
+            <div className="Project-dot" style={{ color: "#0EA5E9" }}>
+              .
+            </div>
+          </Content>
+          <div
+            style={{
+              position: "relative",
+              right: "12rem",
+              top: "12rem",
+              transform: "rotate(180deg)",
+            }}
+          >
+            <img src="/arrow.png" />
+          </div>
+        </Content>
+      </Content>
+    </Content>
+  );
+}
+
+export default Project;
